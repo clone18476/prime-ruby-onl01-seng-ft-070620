a7c3e76eff4c47294f281f4ed5fb06f3b97043e2
Cameron Cousins <cameroncousins18@gmail.com>
2020-07-16 04:18:07 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(num)
+  n = 2
+  while n < num
+    return false if num % n == 0
+  end
+  true
+  while n < num 
+    return true if num % n == 0 
+    n += 1
+  end
+  false
+end
